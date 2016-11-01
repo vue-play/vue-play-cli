ca2ffcacb31d1adbae62c1a1ace913b2e879851a
EGOIST <0x142857@gmail.com>
2016-11-01 17:25:22 +0800

    make development server work

--- bin/vue-play-start.js
+++ bin/vue-play-start.js
@@ -1,5 +1,23 @@
 'use strict'
+const webpack = require('webpack')
+const Server = require('webpack-dev-server')
+const makeConfig = require('../lib/make-config')
 
 module.exports = (input, flags) => {
-
+  const options = Object.assign({
+    type: 'start',
+    port: 5000
+  }, flags)
+  const config = makeConfig(options)
+  const app = new Server(webpack(config), {
+    stats: {
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    }
+  })
+  app.listen(options.port)
+  console.log(`> http://localhost:${options.port}`)
 }
