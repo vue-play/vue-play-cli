44eb1d13d51d23d9b480edff7209ac2efab844cb
EGOIST <0x142857@gmail.com>
2016-11-02 17:47:40 +0800

    add more loaders

--- bin/vue-play-start.js
+++ bin/vue-play-start.js
@@ -1,6 +1,6 @@
 'use strict'
 const webpack = require('webpack')
-const server = require('webpack-hot-server')
+const server = require('../lib/server')
 const makeConfig = require('../lib/make-config')
 
 module.exports = (input, flags) => {
@@ -9,19 +9,5 @@ module.exports = (input, flags) => {
     port: 5000
   }, flags)
   const config = makeConfig(options)
-  const app = server({
-    config,
-    webpack,
-    hot: true,
-    customIndex: true,
-    stats: {
-      colors: true,
-      modules: false,
-      children: false,
-      chunks: false,
-      chunkModules: false
-    }
-  })
-  app.listen(options.port)
-  console.log(`> http://localhost:${options.port}`)
+  server(config, options)
 }
